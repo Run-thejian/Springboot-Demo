d287ad89972a71a87b7d5d2b861ee6e0b9d3c570
zjy <1571367814@qq.com>
2023-10-20 10:18:46 +0800

    thymeleaf初试

---
+++ src/main/java/com/zhong/controller/IndexController.java
@@ -0,0 +1,22 @@
+package com.zhong.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author ：zhongjianyi
+ * @description：TODO
+ * @date ：2023-10-20 10:14
+ */
+
+//在templates目录下的所有页面，只能通过controller来跳转
+//这个需要模板引擎的支持！thymeleaf
+
+@Controller
+public class IndexController {
+
+    @RequestMapping("/test")
+    public String test(){
+        return "test";
+    }
+}
